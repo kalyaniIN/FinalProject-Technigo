e565e6fc6fdc5b98fa985e15c7cae488f7b62553
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-12-05 14:00:07 +0100

    - Added apis folder to have API logics and local storage logic separately. This ensures some what clean code. - Stored API results in browser's local storage so that we won't run out of 150 points limit we have in free api. - Added addRecipeNutrition=true in search api query string so that we can avoid calling another API 'https://api.spoonacular.com/recipes/${recipeId}/nutritionWidget.json?apiKey=${Api_Key}' to fetch these information.

--- src/components/Category/Vegetarian.jsx
+++ src/components/Category/Vegetarian.jsx
@@ -1,18 +1,19 @@
-// import {useEffect } from "react";
+import { useEffect } from "react";
+import { getVegetarianKeto } from "../../apis/fetchRecipes";
 
 export const Vegetarian = () => {
-    // const Api_Key = "dca7e0f9f77c400aa4cb72b57e168d23";
-    // const getVegetarian = async () => {
-    //   const api = await fetch(
-    //     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${Api_Key}&diet=vegetarian&number=20`
-    //   );
-    //   const data = await api.json();
-    //   console.log(data);
-    // };
-    // useEffect(() => {
-    //     getVegetarian();
-    //   }, []);
-    return (
-      <div>vegetarian</div>
-    )
-  }
+  const getVegetarianList = async () => {
+    try {
+      const data = await getVegetarianKeto();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getVegetarianList();
+  }, []);
+
+  return <div>vegetarian</div>;
+};
