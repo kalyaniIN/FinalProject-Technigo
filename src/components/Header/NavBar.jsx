be1cc47f56ac4e2ad3fdd05a6b8ba1e931a54f77
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-12-08 21:54:15 +0100

    Hide the favorites heart button in the favorites page. Show favorites link only when some favorites available

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,21 +1,27 @@
 import { MdFoodBank } from "react-icons/md";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./NavBar.css";
 
-export const NavBar = () => (
-  <nav>
-    <div className="nav-bar">
-      <div></div>
-      <div className="nav-bar-brand">
-        <NavLink className="head-text" to="/">
-          <MdFoodBank />
-          <span className="nav-bar-brand-text">BalancedBites</span>
-        </NavLink>
+export const NavBar = () => {
+  const { favoriteRecipeItems } = useSelector((state) => state.recipe);
+  return (
+    <nav>
+      <div className="nav-bar">
+        <div></div>
+        <div className="nav-bar-brand">
+          <NavLink className="head-text" to="/">
+            <MdFoodBank />
+            <span className="nav-bar-brand-text">BalancedBites</span>
+          </NavLink>
+        </div>
+        <div className="nav-bar-favorites">
+          {favoriteRecipeItems.length > 0 && (
+            <NavLink to="/favorites">Favorites</NavLink>
+          )}
+        </div>
       </div>
-      <div className="nav-bar-favorites">
-        <NavLink to="/favorites">Favorites</NavLink>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
