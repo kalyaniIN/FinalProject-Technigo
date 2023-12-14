75713583dfb91f931b1b36ade3f24c1b99bd75f7
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-12-14 22:59:33 +0100

    Added title for favorites list

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,25 @@
 import { useSelector } from "react-redux";
 
+import styled from "styled-components";
+
 import RecipeList from "../components/RecipeList";
 
 export const Favorites = () => {
   const { favoriteRecipeItems } = useSelector((state) => state.recipe);
 
-  return <RecipeList recipeList={favoriteRecipeItems} />;
+  return (
+    <Wrapper>
+      <Title>Favorite recipes:</Title>
+      <RecipeList recipeList={favoriteRecipeItems} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  margin-top: 2rem;
+`;
+
+const Title = styled.h2`
+  text-align: left;
+  margin-bottom: 10px;
+`;
